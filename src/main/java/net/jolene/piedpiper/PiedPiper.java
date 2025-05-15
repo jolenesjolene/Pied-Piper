6d2a3130f3f2f6925633c5e04af1118cc1f481df
jolenesjolene <imfullof137@gmail.com>
2025-05-15 21:28:35 +0100

    Initial commit

---
+++ src/main/java/net/jolene/piedpiper/PiedPiper.java
@@ -0,0 +1,12 @@
+package net.jolene.piedpiper;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class PiedPiper implements ModInitializer {
+	public static final String MOD_ID = "piedpiper";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	@Override
+	public void onInitialize() {
+	}
+}
