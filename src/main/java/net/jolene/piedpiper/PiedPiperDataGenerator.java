b43ccceb26ed14359c93f4abe528c96bf371f0d5
Jolene <lloydjoseph137@gmail.com>
2025-10-14 22:47:03 +0100

    Initial commit

---
+++ src/main/java/net/jolene/piedpiper/PiedPiperDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jolene.piedpiper;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PiedPiperDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
