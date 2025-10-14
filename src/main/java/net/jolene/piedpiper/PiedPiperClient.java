fb0a87887b0efac4c7ffb6d4123e29bcf22da622
Jolene <lloydjoseph137@gmail.com>
2025-10-14 23:00:57 +0100

    Added PiedPiperClient.java and description and authors to fabric.mod.json

---
+++ src/main/java/net/jolene/piedpiper/PiedPiperClient.java
@@ -0,0 +1,10 @@
+package net.jolene.piedpiper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PiedPiperClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
